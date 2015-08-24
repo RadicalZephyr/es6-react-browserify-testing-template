997e025f06ac2e39028cb7391fc3d66a9682b4c7
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-24 15:17:30 -0500

    Reindent javascript task

--- gulpfile.js
+++ gulpfile.js
@@ -12,43 +12,43 @@ var sourcemaps = require('gulp-sourcemaps');
 var babelify = require('babelify');
 
 gulp.task('javascript', function () {
-  // gulp expects tasks to return a stream, so we create one here.
-  var bundledStream = through();
+    // gulp expects tasks to return a stream, so we create one here.
+    var bundledStream = through();
 
-  bundledStream
+    bundledStream
     // turns the output bundle stream into a stream containing
     // the normal attributes gulp plugins expect.
-    .pipe(source('app.js'))
+        .pipe(source('app.js'))
     // the rest of the gulp task, as you would normally write it.
     // here we're copying from the Browserify + Uglify2 recipe.
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-      // Add gulp plugins to the pipeline here.
-      .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/js/'));
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+    // Add gulp plugins to the pipeline here.
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/'));
 
-  // "globby" replaces the normal "gulp.src" as Browserify
-  // creates it's own readable stream.
+    // "globby" replaces the normal "gulp.src" as Browserify
+    // creates it's own readable stream.
     globby(['./src/**/*.js', './test/**/*.js'], function(err, entries) {
-    // ensure any errors from globby are handled
-    if (err) {
-      bundledStream.emit('error', err);
-      return;
-    }
+        // ensure any errors from globby are handled
+        if (err) {
+            bundledStream.emit('error', err);
+            return;
+        }
 
-    // create the Browserify instance.
-    var b = browserify({
-      entries: entries,
-      debug: true,
-      transform: [babelify]
+        // create the Browserify instance.
+        var b = browserify({
+            entries: entries,
+            debug: true,
+            transform: [babelify]
+        });
+
+        // pipe the Browserify stream into the stream we created earlier
+        // this starts our gulp pipeline.
+        b.bundle().pipe(bundledStream);
     });
 
-    // pipe the Browserify stream into the stream we created earlier
-    // this starts our gulp pipeline.
-    b.bundle().pipe(bundledStream);
-  });
-
-  // finally, we return the stream, so gulp knows when this task is done.
-  return bundledStream;
+    // finally, we return the stream, so gulp knows when this task is done.
+    return bundledStream;
 });
